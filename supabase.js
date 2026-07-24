61e99eb1d4fb63e0f332cb1005ca7bdee985cb4f
just9yousef <yasah2024@gmail.com>
2026-07-25 03:29:11 +0400

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://lhnxclfimtqrsznlygty.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_r-CCgm4wS0gOIa6_OOOzOg_MdbEkmcr";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
