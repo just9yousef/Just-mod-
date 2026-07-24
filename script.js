f2bad0e543090b6d7719614fda90daf18644a387
just9yousef <yasah2024@gmail.com>
2026-07-25 03:59:51 +0400

    script.js

--- script.js
+++ script.js
@@ -30,3 +30,62 @@ async function login() {
     alert("Login successful!");
   }
 }
+async function uploadMod(){
+
+const file = document.getElementById("file").files[0];
+const name = document.getElementById("name").value;
+const version = document.getElementById("version").value;
+const description = document.getElementById("description").value;
+
+
+if(!file){
+alert("Choose a file first");
+return;
+}
+
+
+const fileName = Date.now() + "-" + file.name;
+
+
+// Upload file
+const {error: uploadError} = await supabaseClient
+.storage
+.from("mods")
+.upload(fileName, file);
+
+
+if(uploadError){
+alert(uploadError.message);
+return;
+}
+
+
+// Get link
+const {data} = supabaseClient
+.storage
+.from("mods")
+.getPublicUrl(fileName);
+
+
+const user = await supabaseClient.auth.getUser();
+
+
+// Save info
+const {error} = await supabaseClient
+.from("mods")
+.insert({
+name:name,
+version:version,
+description:description,
+file_url:data.publicUrl,
+creator:user.data.user.id
+});
+
+
+if(error){
+alert(error.message);
+}else{
+alert("Mod uploaded successfully!");
+}
+
+}
