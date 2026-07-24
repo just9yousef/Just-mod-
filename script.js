8f930e2abcc722d84273bfb555bb850a2395721b
just9yousef <yasah2024@gmail.com>
2026-07-25 02:16:23 +0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const search = document.querySelector(".hero input");
+const cards = document.querySelectorAll(".card");
 
+search.addEventListener("input", () => {
+  const text = search.value.toLowerCase();
+
+  cards.forEach(card => {
+    const name = card.textContent.toLowerCase();
+
+    if (name.includes(text)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
